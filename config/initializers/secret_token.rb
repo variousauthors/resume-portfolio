8da7f7d183f617a6e0918f17be722b0eedc1da0d
Zeigfreid <various.authors@gmail.com>
2013-04-07 15:00:30 -0700

    Installed haml and high_voltage
    Made static pages for SAP and wewanttoknow

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StyledPortfolio::Application.config.secret_token = 'd39db9506fb8dd55f57ab7c68bd151d704a0f9ea71175c725f4f6cb54014d39f82aebd236f53303018a653ff61bfb378c7a7aa95b6391fd24319a1a44741d0d9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+StyledPortfolio::Application.config.session_store :cookie_store, key: '_styled_portfolio_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# StyledPortfolio::Application.config.session_store :active_record_store
